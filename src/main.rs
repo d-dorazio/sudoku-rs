45be23b92aad95f18c68bfa678af12ae84cb813a
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-02-07 23:09:20 +0100

    add timing logs in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::env;
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
+use std::time::Instant;
+
+use sudoku_rs::Sudoku;
 
 fn main() -> io::Result<()> {
     let fp = env::args()
@@ -11,19 +14,30 @@ fn main() -> io::Result<()> {
     let f = File::open(fp)?;
     let buf = BufReader::new(f);
 
-    for (i, l) in buf.lines().enumerate() {
-        let l = l?;
+    let sudokus = buf
+        .lines()
+        .map(|l| Sudoku::from_line(&l.unwrap()).unwrap())
+        .collect::<Vec<_>>();
 
-        let sudoku = sudoku_rs::Sudoku::from_line(&l).unwrap();
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
 
+    let start_t = Instant::now();
+    for (i, sudoku) in sudokus.into_iter().enumerate() {
         let solution = sudoku.first_solution();
         let is_solved = solution.map_or(false, |s| s.is_solved());
-        println!("#{} is solved {:?}", i, is_solved);
+
+        writeln!(stdout, "#{} is solved {:?}", i, is_solved)?;
 
         if !is_solved {
-            panic!();
+            panic!(
+                "all input sudoku should be solvable but #{} is not: {:?}",
+                i, sudoku
+            );
         }
     }
+
+    println!("total time elapsed {:?}", start_t.elapsed());
 
     Ok(())
 }
