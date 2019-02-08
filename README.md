848e2fddd323b96752b58e05cb6a03aacd56aa3e
Daniele D'Orazio <d.dorazio96@gmail.com>
2019-02-09 00:46:59 +0100

    refine main to support both sudoku generation and solving

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Sudoku solver and generator in Rust to explore benchmarking and profiling. It
 is quite fast.
+
+## Usage
+
+```shell
+$ cargo run --release -- solve data/sudoku17.txt
+$ cargo run --release -- generate --count 15000 --free-cells 60
+```
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,71 @@
-use std::env;
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Read, Write};
+use std::path::PathBuf;
 use std::time::Instant;
+
+use structopt::StructOpt;
 
 use sudoku_rs::Sudoku;
 
+/// Just a sudoku solver and generator.
+#[derive(StructOpt, Debug)]
+enum Cmd {
+    /// Generate a given number of solvable sudoku. Uniqueness is not enforced.
+    #[structopt(name = "generate")]
+    Generate {
+        /// How many free cells each generated sudoku should have.
+        #[structopt(short = "f", long = "free-cells")]
+        free_cells: usize,
+
+        /// How many sudoku to generate.
+        #[structopt(short = "c", long = "count")]
+        count: usize,
+    },
+
+    /// Solve all the sudoku from the given file or input printing the total
+    /// time elapsed.
+    #[structopt(name = "solve")]
+    Solve {
+        #[structopt(parse(from_os_str))]
+        sudoku: Option<PathBuf>,
+    },
+}
+
 fn main() -> io::Result<()> {
-    let fp = env::args()
-        .nth(1)
-        .expect("please provide an input sudoku archive");
+    let cmd = Cmd::from_args();
 
-    let f = File::open(fp)?;
-    let buf = BufReader::new(f);
+    let stdout = io::stdout();
+    let stdout = stdout.lock();
 
-    let sudokus = buf
+    match cmd {
+        Cmd::Generate { free_cells, count } => generate_sudoku(free_cells, count, stdout),
+        Cmd::Solve { sudoku: Some(p) } => {
+            let f = File::open(p)?;
+            solve_sudoku(f, stdout)
+        }
+        Cmd::Solve { sudoku: None } => {
+            let stdin = io::stdin();
+            let stdin = stdin.lock();
+            solve_sudoku(stdin, stdout)
+        }
+    }
+}
+
+fn solve_sudoku(r: impl Read, mut out: impl Write) -> io::Result<()> {
+    let buf = BufReader::new(r);
+
+    let sudoku = buf
         .lines()
         .map(|l| Sudoku::from_line(&l.unwrap()).unwrap())
         .collect::<Vec<_>>();
 
-    let stdout = io::stdout();
-    let mut stdout = stdout.lock();
-
     let start_t = Instant::now();
-    for (i, sudoku) in sudokus.into_iter().enumerate() {
+    for (i, sudoku) in sudoku.into_iter().enumerate() {
         let solution = sudoku.first_solution();
         let is_solved = solution.map_or(false, |s| s.is_solved());
 
-        writeln!(stdout, "#{} is solved {:?}", i, is_solved)?;
+        writeln!(out, "#{} is solved {:?}", i, is_solved)?;
 
         if !is_solved {
             panic!(
@@ -37,7 +75,18 @@ fn main() -> io::Result<()> {
         }
     }
 
-    println!("total time elapsed {:?}", start_t.elapsed());
+    writeln!(out, "total time elapsed {:?}", start_t.elapsed())
+}
+
+fn generate_sudoku(free_cells: usize, count: usize, mut out: impl Write) -> io::Result<()> {
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..count {
+        let sudoku = Sudoku::generate_solvable(&mut rng, free_cells)
+            .expect("cannot create a solvable sudoku");
+
+        writeln!(out, "{}", sudoku.to_line())?;
+    }
 
     Ok(())
 }
